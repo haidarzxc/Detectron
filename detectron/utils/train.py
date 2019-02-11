0205b5ebf594eca94c4840145d501d748bad3d11
haidarzxc <haidar.a.alanbari@gmail.com>
2019-02-10 19:48:58 -0500

    zcv

--- detectron/utils/train.py
+++ detectron/utils/train.py
@@ -190,6 +190,7 @@ def add_model_training_inputs(model):
     """Load the training dataset and attach the training inputs to the model."""
     logger = logging.getLogger(__name__)
     logger.info('Loading dataset: {}'.format(cfg.TRAIN.DATASETS))
+    print("HEE HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
     roidb = combined_roidb_for_training(
         cfg.TRAIN.DATASETS, cfg.TRAIN.PROPOSAL_FILES
     )
