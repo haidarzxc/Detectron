8e5083f7a0ef05b152b2b67a9449cdf7438ed4ac
haidarzxc <haidar.a.alanbari@gmail.com>
2019-02-10 19:53:35 -0500

    zcv

--- detectron/utils/train.py
+++ detectron/utils/train.py
@@ -186,7 +186,7 @@ def setup_model_for_training(model, weights_file, output_dir):
 
 
 def add_model_training_inputs(model):
-    print("HEE HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+    logger.info("hhhhhhhhhhhhhhhhhhhhh")
     """Load the training dataset and attach the training inputs to the model."""
     logger = logging.getLogger(__name__)
     logger.info('Loading dataset: {}'.format(cfg.TRAIN.DATASETS))
